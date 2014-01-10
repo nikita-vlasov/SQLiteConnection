0a83150f810284e64d90728d983915352a67bfb0
Alex <alex.kovalskiy@icloud.com>
2014-01-10 20:39:21 +0200

    Initial Commit

---
+++ Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Alex on 1/10/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
